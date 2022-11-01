af326a4d797ef552a5719ed926af24ce82b0252f
jemmy <flashyjotebele@gmail.com>
2022-11-01 10:34:00 -1100

    design-done

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,16 @@ module.exports = {
     fontFamily: {
       sans: ["DM Sans", "sans-serif"],
     },
-    colors: {
-      softRed: "hsl(10,79%, 65%)",
-      Cyan: "hsl(186, 34%, 60%)",
-      darkBrown: "hsl(25, 47%, 15%)",
-      mediumBrown: "hsl(28, 10%, 53%)",
-      Cream: "hsl(27, 66%, 92%)",
-      veryPaleOrange: "hsl(33,100%,98%)",
+    extend: {
+      colors: {
+        softRed: "hsl(10,79%, 65%)",
+        Cyan: "hsl(186, 34%, 60%)",
+        darkBrown: "hsl(25, 47%, 15%)",
+        mediumBrown: "hsl(28, 10%, 53%)",
+        Cream: "hsl(27, 66%, 92%)",
+        veryPaleOrange: "hsl(33,100%,98%)",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
